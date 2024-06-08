b9ca7b9f9478b4c3bb68519215c034957b1574ae
Max <102813400+Seiigfiied@users.noreply.github.com>
2024-06-08 20:38:49 +0300

    Upd

--- Course-work-Back-end/src/main/java/com/chesnakov/hardwareecommerce/SecondHandECommerceApplication.java
+++ Course-work-Back-end/src/main/java/com/chesnakov/hardwareecommerce/SecondHandECommerceApplication.java
@@ -2,6 +2,9 @@ package com.chesnakov.hardwareecommerce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class SecondHandECommerceApplication {
@@ -10,4 +13,13 @@ public class SecondHandECommerceApplication {
         SpringApplication.run(SecondHandECommerceApplication.class, args);
     }
 
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("http://46.253.143.107:4200").allowedMethods("*");
+            }
+        };
+    }
 }
